ebf101e0179a817a78608c86b121878d6564cdcf
Nazar Kundys <nazarrisd@gmail.com>
2026-03-08 15:26:33 +0200

    ✨ feat(game): implement 2048 game logic with move, spawn, and win detection
    - add move methods (left/right/up/down) using `slideRow` and `transposeBoard` approach
    - add tile spawning, win/lose detection via `STATUS` enum, and score tracking
    - switch script tag to `type="module"` in **index.html**
    - update body background color in **main.scss**

--- src/modules/Game.class.js
+++ src/modules/Game.class.js
@@ -1,11 +1,15 @@
 'use strict';
 
-/**
- * This class represents the game.
- * Now it has a basic structure, that is needed for testing.
- * Feel free to add more props and methods if needed.
- */
+const STATUS = Object.freeze({
+  IDLE: 'idle',
+  PLAYING: 'playing',
+  WIN: 'win',
+  LOSE: 'lose',
+});
+
 class Game {
+  BOARD_SIZE = 4;
+
   /**
    * Creates a new game instance.
    *
@@ -20,25 +24,75 @@ class Game {
    * If passed, the board will be initialized with the provided
    * initial state.
    */
-  constructor(initialState) {
-    // eslint-disable-next-line no-console
-    console.log(initialState);
+  constructor(
+    initialState = [
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+    ],
+  ) {
+    this.initialState = initialState;
+    this.state = structuredClone(initialState);
+    this.score = 0;
+    this.status = STATUS.IDLE;
   }
 
-  moveLeft() {}
-  moveRight() {}
-  moveUp() {}
-  moveDown() {}
+  moveLeft() {
+    if (this.status !== STATUS.PLAYING) {
+      return;
+    }
+
+    this.state = this.state.map((row) => this.slideRow(row));
+
+    this.afterMove();
+  }
+
+  moveRight() {
+    if (this.status !== STATUS.PLAYING) {
+      return;
+    }
+
+    this.state = this.state.map(
+      (row) => this.slideRow(row.toReversed()).reverse(),
+      // eslint-disable-next-line function-paren-newline
+    );
+
+    this.afterMove();
+  }
+  moveUp() {
+    if (this.status !== STATUS.PLAYING) {
+      return;
+    }
+
+    this.state = this.transposeBoard(this.state);
+    this.moveLeft();
+    this.state = this.transposeBoard(this.state);
+  }
+
+  moveDown() {
+    if (this.status !== STATUS.PLAYING) {
+      return;
+    }
+
+    this.state = this.transposeBoard(this.state);
+    this.moveRight();
+    this.state = this.transposeBoard(this.state);
+  }
 
   /**
    * @returns {number}
    */
-  getScore() {}
+  getScore() {
+    return this.score;
+  }
 
   /**
    * @returns {number[][]}
    */
-  getState() {}
+  getState() {
+    return this.state;
+  }
 
   /**
    * Returns the current game status.
@@ -50,19 +104,115 @@ class Game {
    * `win` - the game is won;
    * `lose` - the game is lost
    */
-  getStatus() {}
+  getStatus() {
+    return this.status;
+  }
 
   /**
    * Starts the game.
    */
-  start() {}
+  start() {
+    this.status = STATUS.PLAYING;
+    this.spawnTile();
+    this.spawnTile();
+  }
 
   /**
    * Resets the game.
    */
-  restart() {}
+  restart() {
+    this.status = STATUS.IDLE;
+    this.score = 0;
+    this.state = structuredClone(this.initialState);
+  }
 
-  // Add your own methods here
+  spawnTile() {
+    const emptyCells = [];
+
+    for (let i = 0; i < this.BOARD_SIZE; i++) {
+      for (let j = 0; j < this.BOARD_SIZE; j++) {
+        if (this.state[i][j] === 0) {
+          emptyCells.push([i, j]);
+        }
+      }
+    }
+
+    if (!emptyCells.length) {
+      return;
+    }
+
+    const [randRow, randCol] =
+      emptyCells[Math.floor(Math.random() * emptyCells.length)];
+
+    this.state[randRow][randCol] = Math.random() < 0.9 ? 2 : 4;
+  }
+
+  slideRow(row) {
+    const skimmed = row.filter((n) => n !== 0);
+    const merged = [];
+
+    for (let i = 0; i < skimmed.length; i++) {
+      if (skimmed[i] === skimmed[i + 1]) {
+        const mergedValue = skimmed[i] * 2;
+
+        merged.push(mergedValue);
+        this.score += mergedValue;
+        i++;
+      } else {
+        merged.push(skimmed[i]);
+      }
+    }
+
+    return merged.concat(Array(this.BOARD_SIZE - merged.length).fill(0));
+  }
+
+  checkMovesLeft() {
+    for (let i = 0; i < this.BOARD_SIZE; i++) {
+      for (let j = 0; j < this.BOARD_SIZE; j++) {
+        if (this.state[i][j] === 0) {
+          return;
+        }
+
+        if (
+          (j + 1 < this.BOARD_SIZE &&
+            this.state[i][j] === this.state[i][j + 1]) ||
+          (i + 1 < this.BOARD_SIZE && this.state[i][j] === this.state[i + 1][j])
+        ) {
+          return;
+        }
+      }
+    }
+
+    this.status = STATUS.LOSE;
+  }
+
+  checkWin() {
+    if (this.state.some((row) => row.some((cell) => cell === 2048))) {
+      this.status = STATUS.WIN;
+    }
+  }
+
+  afterMove() {
+    this.spawnTile();
+    this.checkWin();
+
+    if (this.status === STATUS.PLAYING) {
+      this.checkMovesLeft();
+    }
+  }
+
+  /**
+   * Returns a new board with the rows and columns transposed.
+   *
+   * @param {number[][]} board
+   * The board to transpose.
+   *
+   * @returns {number[][]}
+   * The transposed board.
+   */
+  transposeBoard(board) {
+    return board[0].map((_, col) => board.map((row) => row[col]));
+  }
 }
 
 module.exports = Game;
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,1 @@
 'use strict';
-
-// Uncomment the next lines to use your game instance in the browser
-// const Game = require('../modules/Game.class');
-// const game = new Game();
-
-// Write your code here
